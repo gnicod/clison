64b1a75e699f352593199a15b4eaad082f80b3cc
gnicod <nicod.ga@gmail.com>
2013-05-31 14:39:05 +0200

    add args 1

--- clison.go
+++ clison.go
@@ -7,34 +7,45 @@ import (
 	"fmt"
 	"log"
 	"encoding/json"
+	"strings"
 )
 
 func main() {
-	pattern := os.Args[1]
+	patterns := ""
+	if len(os.Args) > 1{
+		patterns = os.Args[1]
+	}
+	pattern := strings.Split(patterns,".")
 	fmt.Println(pattern)
 
 	in := bufio.NewReader(os.Stdin);
 	input  := ""
 	for {
-		in, err := in.ReadString('\n');
+		in, err := in.ReadString('\n')
 		input += in
 		if err != nil {
-			fmt.Println(parse_json(input,pattern))
+			lol := parse_json(input,pattern,0)
+			os.Stdout.Write(lol)
 			//fmt.Println(input)
 			os.Exit(0);
 		}
 	}
 }
 
-func parse_json(input string,pattern string)  string{
+func parse_json(input string,pattern []string, lvl int )  []byte{
 	var j map[string]interface{}
 	err := json.Unmarshal([]byte(input), &j)
 	if err != nil {
 		panic(err)
 	}
-	out,err := json.Marshal(j["menu"])
+	//out,err := json.MarshalIndent(j["response"],"","\t")
+	out,err := json.MarshalIndent(j[pattern[lvl]],"","\t")
 	if err != nil{
 		log.Fatal(err)
 	}
-	return string(out)
+	lvl += 1
+	if lvl < len(pattern){
+		return parse_json(string(out),pattern,lvl)
+	}
+	return out
 }
