a6ff9dc32ec4d26a3d0fc094f305b448b5ed816c
gnicod <nicod.ga@gmail.com>
2013-06-14 17:56:10 +0200

    filter

--- clison.go
+++ clison.go
@@ -5,9 +5,11 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"bytes"
 	"encoding/json"
 	"strings"
 	"strconv"
+	"regexp"
 )
 
 func main() {
@@ -28,14 +30,16 @@ func main() {
 			i := 0
 			if !hazArg { i=-1 }
 			lol := parse_json(input,pattern,i)
-			os.Stdout.Write(lol)
+			//if starts with and ends with "
+			os.Stdout.Write(bytes.TrimSuffix(bytes.TrimPrefix(lol, []byte("\"")), []byte("\""))) 
 			//fmt.Println(input)
-			os.Exit(0);
+			os.Exit(0)
 		}
 	}
 }
 
 func parse_json(input string,pattern []string, lvl int )  []byte{
+	fmt.Println(pattern[lvl])
 	var gOut  []byte
 	var f interface{}
 	err := json.Unmarshal([]byte(input), &f)
@@ -54,24 +58,30 @@ func parse_json(input string,pattern []string, lvl int )  []byte{
 					return parse_json(string(out),pattern,lvl)
 				}
     		case []interface{}:
-				//cast string to i
-				convi, err := strconv.Atoi(pattern[lvl])
-				if err != nil {
-					//TODO print a more usefull message
-					fmt.Println(err)
-					os.Exit(2)
+				//if match pattern [name="Bob"]
+				var searchPatternRegexp = regexp.MustCompile("\\[(.*?)=(.*?)\\]")
+				if searchPatternRegexp.MatchString(pattern[lvl]) {
+					split      := searchPatternRegexp.FindStringSubmatch(pattern[lvl])
+					searchInArray(vf,split[1],split[2])
+				}else{
+					//cast string to i
+					convi, err := strconv.Atoi(pattern[lvl])
+					if err != nil {
+						//TODO print a more usefull message
+						fmt.Println(err)
+						os.Exit(2)
+					}
+					out,err := json.MarshalIndent(vf[convi],"","\t")
+					gOut = out
+					if err != nil{
+						log.Fatal(err)
+					}
+					lvl += 1
+					if lvl < len(pattern){
+						return parse_json(string(out),pattern,lvl)
+					}
 				}
-				//
-				searchInArray(vf,"lol","lol")
-				out,err := json.MarshalIndent(vf[convi],"","\t")
-				gOut = out
-				if err != nil{
-					log.Fatal(err)
-				}
-				lvl += 1
-				if lvl < len(pattern){
-					return parse_json(string(out),pattern,lvl)
-				}
+
 		}
 	}
 	return gOut
@@ -82,7 +92,12 @@ func searchInArray(arr []interface{},keyPat string, valuePat string){
 	fmt.Println(keyPat,valuePat)
 	for k, v := range arr {
 		//check if it's a map
-		fmt.Println(k,v)
+		fmt.Println(k)
+		fmt.Println(v)
+		//for kk, vv := range arr {
+		//}
+		var v1 = reflect.Type = reflect.TypeOf((*v)(nil)).Elem()
+		fmt.Println(v1)
 		//strip all about pattern
 	}
 	fmt.Println(found)
