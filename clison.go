d13bbe7c6e54865e30b4ed6fc0bf1ba1dfb2b7d5
gnicod <nicod.ga@gmail.com>
2014-03-26 14:33:28 +0100

    go fmt

--- clison.go
+++ clison.go
@@ -1,115 +1,116 @@
 package main
 
 import (
-	"os"
 	"bufio"
-	"fmt"
-	"log"
 	"bytes"
 	"encoding/json"
-	"strings"
-	"strconv"
+	"fmt"
+	"log"
+	"os"
 	"regexp"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	patterns := ""
 	hazArg := false
-	if len(os.Args) > 1{
+	if len(os.Args) > 1 {
 		patterns = os.Args[1]
 		hazArg = true
 	}
-	pattern := strings.Split(patterns,".")
+	pattern := strings.Split(patterns, ".")
 
-	in := bufio.NewReader(os.Stdin);
-	input  := ""
+	in := bufio.NewReader(os.Stdin)
+	input := ""
 	for {
 		in, err := in.ReadString('\n')
 		input += in
 		if err != nil {
 			i := 0
-			if !hazArg { i=-1 }
-			lol := parse_json(input,pattern,i)
+			if !hazArg {
+				i = -1
+			}
+			lol := parse_json(input, pattern, i)
 			//if starts with and ends with "
-			os.Stdout.Write(bytes.TrimSuffix(bytes.TrimPrefix(lol, []byte("\"")), []byte("\""))) 
+			os.Stdout.Write(bytes.TrimSuffix(bytes.TrimPrefix(lol, []byte("\"")), []byte("\"")))
 			os.Exit(0)
 		}
 	}
 }
 
-func parse_json(input string,pattern []string, lvl int )  []byte{
+func parse_json(input string, pattern []string, lvl int) []byte {
 	//if no pattern
-	if lvl==-1{
+	if lvl == -1 {
 		var iface interface{}
 		err := json.Unmarshal([]byte(input), &iface)
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
-		indented,err := json.MarshalIndent(iface,"","\t")
+		indented, err := json.MarshalIndent(iface, "", "\t")
 		//we indent then exit
-		os.Stdout.Write(bytes.TrimSuffix(bytes.TrimPrefix(indented, []byte("\"")), []byte("\""))) 
+		os.Stdout.Write(bytes.TrimSuffix(bytes.TrimPrefix(indented, []byte("\"")), []byte("\"")))
 		os.Exit(0)
 	}
 
-	var gOut  []byte
+	var gOut []byte
 	var f interface{}
 	err := json.Unmarshal([]byte(input), &f)
 	if err != nil {
 		panic(err)
-	}else{
+	} else {
 		switch vf := f.(type) {
-			case map[string]interface{}:
-				out,err := json.MarshalIndent(vf[pattern[lvl]],"","\t")
+		case map[string]interface{}:
+			out, err := json.MarshalIndent(vf[pattern[lvl]], "", "\t")
+			gOut = out
+			if err != nil {
+				log.Fatal(err)
+			}
+			lvl += 1
+			if lvl < len(pattern) {
+				return parse_json(string(out), pattern, lvl)
+			}
+		case []interface{}:
+			//if match pattern [name="Bob"]
+			var searchPatternRegexp = regexp.MustCompile("\\[(.*?)=(.*?)\\]")
+			if searchPatternRegexp.MatchString(pattern[lvl]) {
+				split := searchPatternRegexp.FindStringSubmatch(pattern[lvl])
+
+				for k, v := range vf {
+					r := v.(map[string]interface{})
+					_ = k
+					if r[split[1]] == split[2] {
+						out, err := json.MarshalIndent(r, "", "\t")
+						if err != nil {
+							log.Fatal(err)
+						}
+						lvl += 1
+						if lvl < len(pattern) {
+							return parse_json(string(out), pattern, lvl)
+						} else {
+							return out
+						}
+					}
+				}
+
+			} else {
+				//cast string to i
+				convi, err := strconv.Atoi(pattern[lvl])
+				if err != nil {
+					//TODO print a more usefull message
+					fmt.Println(err)
+					os.Exit(2)
+				}
+				out, err := json.MarshalIndent(vf[convi], "", "\t")
 				gOut = out
-				if err != nil{
+				if err != nil {
 					log.Fatal(err)
 				}
 				lvl += 1
-				if lvl < len(pattern){
-					return parse_json(string(out),pattern,lvl)
+				if lvl < len(pattern) {
+					return parse_json(string(out), pattern, lvl)
 				}
-			case []interface{}:
-				//if match pattern [name="Bob"]
-				var searchPatternRegexp = regexp.MustCompile("\\[(.*?)=(.*?)\\]")
-				if searchPatternRegexp.MatchString(pattern[lvl]) {
-					split      := searchPatternRegexp.FindStringSubmatch(pattern[lvl])
-
-					for k, v := range vf {
-						r := v.(map[string]interface{})
-						_ = k
-						if r[split[1]]==split[2]{
-							out,err := json.MarshalIndent(r,"","\t")
-							if err != nil{
-								log.Fatal(err)
-							}
-							lvl += 1
-							if lvl < len(pattern){
-								return parse_json(string(out),pattern,lvl)
-							}else{
-								return out
-							}
-						}
-					}
-
-
-				}else{
-					//cast string to i
-					convi, err := strconv.Atoi(pattern[lvl])
-					if err != nil {
-						//TODO print a more usefull message
-						fmt.Println(err)
-						os.Exit(2)
-					}
-					out,err := json.MarshalIndent(vf[convi],"","\t")
-					gOut = out
-					if err != nil{
-						log.Fatal(err)
-					}
-					lvl += 1
-					if lvl < len(pattern){
-						return parse_json(string(out),pattern,lvl)
-					}
-				}
+			}
 		}
 	}
 	return gOut
